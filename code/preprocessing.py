3cd776154e8ec9d4d5ab5bc3847c3c581cfc9d8d
sazzle2 <azheng@umass.edu>
2024-05-10 16:36:14 -0400

    label

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -12,11 +12,14 @@ df.replace(' ?', None, inplace=True)
 # Write the modified DataFrame to a new CSV file
 df.to_csv(output_file, index=False, header=False)
 
-married_attr = [' Widowed', ' Separated', ' Married-civ-spouse', ' Married-spouse-absent', ' Married-AF-spouse']
+married_attr = [' Widowed', ' Separated', ' Married-civ-spouse', ' Married-spouse-absent', ' Married-AF-spouse', ' Never-married']
 unmarried_attr = [' Divorced',' Never-married']
 
 marital_status = df.iloc[:, 5]
 for m in married_attr:
 	marital_status[marital_status == m] = 'Married'
 for u in married_attr:
-	marital_status[marital_status == u] = 'U'
+	marital_status[marital_status == u] = 'Unmarried'
+
+df.head()
+df.to_csv('../data/processed.csv', index=False, header=False)
