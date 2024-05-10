a498a53b59fd40d9839eaa473ec111d4befd7499
sazzle2 <azheng@umass.edu>
2024-05-10 17:11:38 -0400

    pre

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -1,25 +1,23 @@
 import pandas as pd
 
 input_file = '../rawdata/adult.data'
-output_file = '../data/fullset.csv'
+output_file = '../data/processed.csv'
+
+headers = ["age", "workclass", "fnlwgt", "education", "education_num", "marital_status",
+           "occupation", "relationship", "race", "sex", "capital_gain", "capital_loss", 
+           "hours_per_week", "native_country", "salary"]
 
 # read the csv
-df = pd.read_csv(input_file, header=None, delimiter=', ')
+df = pd.read_csv(input_file, header=0, names = headers, delimiter=', ')
 
 # replace all ? as None in the db
 df.replace('?', None, inplace=True)
 
-# Write the modified DataFrame to a new CSV file
-df.to_csv(output_file, index=False, header=False)
-
 married_attr = ['Widowed', 'Separated', 'Married-civ-spouse', 'Married-spouse-absent', 'Married-AF-spouse', 'Never-married']
 unmarried_attr = ['Divorced','Never-married']
 
-marital_status = df.iloc[:, 5]
 for m in married_attr:
-	marital_status[marital_status == m] = 'Married'
+	df['marital_status'] = df['marital_status'].replace(m, 'Married')
 for u in married_attr:
-	marital_status[marital_status == u] = 'Unmarried'
-
-df.head()
+	df['marital_status'] = df['marital_status'].replace(u, 'Unmarried')
 df.to_csv('../data/processed.csv', index=False, header=False)
