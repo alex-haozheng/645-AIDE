b7f43062cd5853aed6c065560c9eda541e676f51
sazzle2 <azheng@umass.edu>
2024-05-10 16:46:07 -0400

    li

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -7,13 +7,13 @@ output_file = '../data/fullset.csv'
 df = pd.read_csv(input_file, header=None, delimiter=', ')
 
 # replace all ? as None in the db
-df.replace(' ?', None, inplace=True)
+df.replace('?', None, inplace=True)
 
 # Write the modified DataFrame to a new CSV file
 df.to_csv(output_file, index=False, header=False)
 
-married_attr = [' Widowed', ' Separated', ' Married-civ-spouse', ' Married-spouse-absent', ' Married-AF-spouse', ' Never-married']
-unmarried_attr = [' Divorced',' Never-married']
+married_attr = ['Widowed', 'Separated', 'Married-civ-spouse', 'Married-spouse-absent', 'Married-AF-spouse', 'Never-married']
+unmarried_attr = ['Divorced','Never-married']
 
 marital_status = df.iloc[:, 5]
 for m in married_attr:
