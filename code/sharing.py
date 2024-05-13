d9da1624a49c03f75e533e50854e893f5eaac4e5
sazzle2 <azheng@umass.edu>
2024-05-13 01:18:54 -0400

    done sharin

--- code/sharing.py
+++ code/sharing.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import psycopg2
+import scipy
 
 
 # TODO change variable to connect to presgresdb
@@ -53,32 +54,68 @@ def generate_queries(A, D):
 	ret = {}
 	for a in A:
 		# 0 is married ; 1 is unmarried
-		q = f"select {a}, {get_aggregate()}, case when marital_status = 'Married' then 0 else 1 end as married from {D} where {a} is not null group by {a}, married"
+		q = f"select {a}, {get_aggregate()}, case when marital_status = 'Married' then 1 else 0 end as married from {D} where {a} is not null group by {a}, married"
 		cur.execute(q)
 		# ret[a] = cur.fetchall()
-		ret[a] = [cur.fetchmany(5),[desc[0] for desc in cur.description]]
+		# ret[a] = [cur.fetchmany(5),[desc[0] for desc in cur.description]]
+		ret[a] = [cur.fetchall(),[desc[0] for desc in cur.description]]
 		conn.commit()
-		break
 	return ret
 	
 d = generate_queries(A, d)
 
-#{a: (m, aggregation... , label)}
-def normalize(q):
-	for k, v in q.items():
-		x = np.array(list(v))
-		print(x[:,0])
-		print(x[:,-1])
-		# for i in range(1, x.shape[1]-1):
-		# 	col = x[:, i]
-		# 	print(col)
+def normalization(arr1, arr2):
+	sum1 = sum(arr1)
+	sum2 = sum(arr2)
+	norm_1 = [elem / sum1 for elem in arr1]
+	norm_2 = [elem / sum2 for elem in arr2]
+
+	return norm_1, norm_2
 	  
-def reformat(q):
+def get_topK(q):
 	ret = {}
 	for a, v in q.items():
 		# get the df with columns labeled
 		df = pd.DataFrame(v[0], columns=v[1])
-		print(df)
-	return 
+		# create separate dataframe for married and unmarried
+		married_df = df[df['married'] == 1]
+		unmarried_df = df[df['married'] == 0]
 
-reformat(d)
+		#merge the two
+		result_df = pd.merge(married_df, unmarried_df, on=a, suffixes=('_md', '_umd'), how='inner')
+		# change to all non zero values 
+		# df.replace(0, 1e-10, inplace=True)
+
+		# .columns returns list of string columsn 
+		for column in result_df.columns[1:31]:
+			#break into strings
+			c = column.split('_')
+			f, m, _ = c[0], '_'.join(c[1:-1]), c[-1]
+			# print(f, m, _)
+			ta = result_df[f'{f}_{m}_md'].astype(float).tolist()
+			tb = result_df[f'{f}_{m}_umd'].astype(float).tolist()
+			for i,t in enumerate(ta):
+				if int(t) == 0:
+					ta[i] = 1e-10
+			for i,t in enumerate(tb):
+				if int(t) == 0:
+					tb[i] = 1e-10
+			n_mar, n_unm = normalization(ta, tb)
+			distance = scipy.stats.entropy(n_mar, n_unm)
+			ret[(a,m,f)] = distance
+	sorted_dict = dict(sorted(ret.items(), key=lambda item: item[1], reverse=True))
+
+	# Get the top 10 items from the sorted dictionary
+	top_10 = dict(list(sorted_dict.items())[:10])
+
+	print("Top 10 dictionary items with highest distance:")
+	for key, value in top_10.items():
+			print(key, "=", value)
+
+	return ret
+
+
+
+#[24(federal gov), 20(self emp), 25(state gov)] [8(fed), 17]
+
+get_topK(d)
