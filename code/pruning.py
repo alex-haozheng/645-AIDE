c34e761f82663b180438f1c88be371d7fbff965f
sazzle2 <azheng@umass.edu>
2024-05-12 20:12:19 -0400

    pruning

--- code/pruning.py
+++ code/pruning.py
@@ -6,3 +6,11 @@ from math import log, pi
 def calc_running_confidence_interval(m, n, d):
 	x = (1-(m-1)/n)*2*log(log(m))+log(pi**2/3*d)
 	return (x/(2*m))**0.5
+
+
+# split dataset #every split call calc_running ... 
+
+# every tuple amf kl-divergence (utility fx) 
+# some tuple amf (if low) get rid of it 
+
+# some tuple amf-amf_1
--- code/sharing.py
+++ code/sharing.py
@@ -14,7 +14,7 @@ cur = conn.cursor()
 cur.execute("select * from adults")
 temp = cur.fetchall()
 conn.commit()
-conn.close()
+# conn.close()
 
 # includes married people
 D_Q = "married"
@@ -22,10 +22,10 @@ D_Q = "married"
 # includes unmarried people
 D_R = "unmarried"
 
-D = [D_Q, D_R]
+d = 'adults'
 
 # dimension attribute (for group by)
-A = ['workclass','education','occupation','relationship','race','sex','native_country','salary_range']
+A = ['workclass','education','occupation','relationship','race','sex','native_country','income']
 # Measure attribute (for aggregate)
 M = ['age','fnlwgt', 'education_num','capital_gain','capital_loss','hours_per_week']
 # aggregate function 
@@ -34,32 +34,39 @@ F = ['MIN', 'MAX', 'COUNT', 'SUM', 'AVG']
 # iterate a, m, f into this query
 # SELECT a, f(m), FROM D group by a
 
-def generate_queries(A, M, F, D_Q, D_R):
-    queries = []
-    for a in A:
-        for m in M:
-            for f in F:
-                query1 = f"SELECT {a}, {f}({m}), FROM {D_Q} GROUP BY {a}"
-                query2 = f"SELECT {a}, {f}{m}), FROM {D_R} GROUP BY {a}"
-                queries.append((query1, query2))
-    return queries
+def get_aggregate():
+	res = ''
+	for f in F:
+		for m in M:
+				res += f'{f}({m}),'
+	return res[:-1]
 
-# queries = generate_queries(A, M, F, D_Q, D_R)
+# print(get_aggregate())
 
-# for query in queries:
-#     print(query)
-# # Unoptimized part 2 exhaustive search
-# def problem_statement():
-#     # iterate through all possible a and m
-    
-#     queries = generate_queries(A, M, F, D_Q, D_R)
-#     for query in queries:
-#         try:
-#             cur = conn.cursor()
-#             cur.execute(query[0])
-#             conn.commit()
-#             conn.close()
-#         except Exception as e:
-#             print(e)
-#         return
+# [a1, f1m1, f2m1, f3m1]
+# [a2, f1m1, f2m1, f3m1]
 
+# select workclass, max(age), case when marital_status = 'Married' then 0 else 1 end as g1, 1 as g2 from adults where workclass is not null group by workclass, g1, g2
+
+def generate_queries(A, D):
+	ret = {}
+	for a in A:
+		# 0 is married ; 1 is unmarried
+		q = f"select {a}, {get_aggregate()}, case when marital_status = 'Married' then 0 else 1 end as married from {D} where {a} is not null group by {a}, married"
+		cur.execute(q)
+		ret[a] = cur.fetchmany(5)
+		break
+	return ret
+	
+d = generate_queries(A, d)
+
+#{a: (m, aggregation... , label)}
+def normalize(q):
+  for k, v in q.items():
+	  x = np.array(list(v))
+	  for i in range(1, x.shape[1]):
+		  col = x[:, i]
+		  print(col)
+	  
+
+normalize(d)
